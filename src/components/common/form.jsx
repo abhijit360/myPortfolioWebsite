114198646b45decc7b464f4ba44cfdab90f97aa9
abhijit <kamathabhijit2002@gmail.com>
2023-07-30 11:01:03 +0530

    Figure out error handling and formsubmission with emailjs implementation. Deployment ready!

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -5,7 +5,7 @@ import "./styles/form.css";
 
 const validate = (values) => {
 	const errors = {};
-	const emailPattern = new RegExp("(w+)@(w+).com");
+	const emailPattern = new RegExp("(\\w+)@(\\w+).com");
 
 	if (!values.name) {
 		errors.name = "Required";
@@ -42,44 +42,55 @@ export const ContactMeForm = () => {
 		},
 		validate,
 		onSubmit: (values) => {
-			alert(JSON.stringify(values, null, 2));
+			// alert(JSON.stringify(values, null, 2));
+			emailjs
+				.sendForm(
+					"service_jvz8sk8",
+					"template_9j2s9sh",
+					form.current,
+					"TAjFzmiiPBeD43mmS"
+				)
+				.then(
+					(result) => {
+						console.log(result.text);
+					},
+					(error) => {
+						console.log(error.text);
+					}
+				);
+
+			formik.setFieldValue("email", "");
+			formik.setFieldTouched('email', false)
+
+			formik.setFieldValue("name", "");
+			formik.setFieldTouched('name', false)
+
+			formik.setFieldValue("subject", "");
+			formik.setFieldTouched('subject', false)
+
+			formik.setFieldValue("message", "");
+			formik.setFieldTouched('message', false)
+
+
+			setFormSubmission(true);
+
+			setTimeout(() => {
+				setFormSubmission(false);
+				console.log("Form Submition set to false");
+			}, 3000);
 		},
 	});
 
-	const sendEmail = (e) => {
-		e.preventDefault();
-		emailjs
-			.sendForm(
-				"service_jvz8sk8",
-				"template_9j2s9sh",
-				form.current,
-				"TAjFzmiiPBeD43mmS"
-			)
-			.then(
-				(result) => {
-					console.log(result.text);
-				},
-				(error) => {
-					console.log(error.text);
-				}
-			);
-
-		setTimeout(() => {
-			setFormSubmission(false);
-		}, 3000);
-
-		formik.setFieldValue("email", "");
-		formik.setFieldValue("name", "");
-		formik.setFieldValue("subject", "");
-		formik.setFieldValue("message", "");
-	};
-
 	return (
 		<div className="contactForm-container">
-			{setFormSubmission && (
+			{formSubmission && (
 				<div className="formSubmissionMessage">Email sent!</div>
 			)}
-			<form ref={form} className="contactForm" onSubmit={sendEmail}>
+			<form
+				ref={form}
+				className="contactForm"
+				onSubmit={formik.handleSubmit}
+			>
 				<div className="contactForm-inputField">
 					<label className="contactForm-label" htmlFor="Name">
 						Your Name:
@@ -154,13 +165,7 @@ export const ContactMeForm = () => {
 						{formik.errors.message}
 					</div>
 				) : null}
-				<button
-					className="contactForm-btn"
-					type="submit"
-					onClick={() => {
-						setFormSubmission(true);
-					}}
-				>
+				<button className="contactForm-btn" type="submit">
 					Send email
 				</button>
 			</form>
