b3c3267584d24b3549ae2dc5a891ab346d91e73b
abhijit <kamathabhijit2002@gmail.com>
2023-07-30 11:12:30 +0530

    Final touches: changing footer copyright and articles -> involvements tab

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -55,7 +55,7 @@ const About = () => {
 								<div className="about-image-container">
 									<div className="about-image-wrapper">
 										<img
-											src="about.jpg"
+											src="AbhijitAboutusPage.jpg"
 											alt="about"
 											className="about-image"
 										/>
